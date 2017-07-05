296e44561e4161efccfbcb2327452460688f0971
atmni5 <aaron.tumini@griffithuni.edu.au>
2017-07-05 14:27:39 +1000

    Online commit

---
+++ Word Reversal/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Word_Reversal
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+		    Program program = new Program();
+			program.Reverse();
+		}
+		
+		private void Reverse(){
+			
+			string input = Console.ReadLine();
+			string[] words = input.Split(' ');
+			string wordsRev = "";
+			string wordRev = "";
+			
+			//iterate through every word in the sentence
+			foreach(string word in words){
+				
+				wordRev = "";
+				
+				//iterate through each character for each word
+				foreach(char c in word){
+					wordRev = wordRev.Insert(0,  c + "");
+				}
+				
+				wordsRev += wordRev + " ";
+				
+			}
+			Console.WriteLine(wordsRev);
+			Console.ReadKey();
+			
+		}
+	}
+}
